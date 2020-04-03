bb912474946ed2894ce98d28510e04da6e9ad9c9
Michal Jarmoc <m.jarmoc@post.pl>
2020-04-03 13:57:50 +0200

    Remove dist from repo

--- dist/redis.service.d.ts
+++
@@ -1,8 +0,0 @@
-import * as Redis from 'ioredis';
-import { RedisClient } from './redis-client.provider';
-export declare class RedisService {
-    private readonly redisClient;
-    constructor(redisClient: RedisClient);
-    getClient(name?: string): Redis.Redis;
-    getClients(): Map<string, Redis.Redis>;
-}
