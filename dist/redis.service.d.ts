e01ae59e785cedcc835cfc14c5db8a2bcbf09f1d
Stanislav V Vyaliy <stas@i-npz.ru>
2020-11-04 16:01:57 +0300

    redis.service.d

--- dist/redis.service.d.ts
+++ dist/redis.service.d.ts
@@ -1,8 +1,8 @@
-import * as Redis from 'ioredis';
+import { Redis } from 'ioredis';
 import { RedisClient } from './redis-client.provider';
 export declare class RedisService {
     private readonly redisClient;
     constructor(redisClient: RedisClient);
-    getClient(name?: string): Redis.Redis;
-    getClients(): Map<string, Redis.Redis>;
+    getClient(name?: string): Redis;
+    getClients(): Map<string, Redis>;
 }
